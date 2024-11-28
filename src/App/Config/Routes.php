c6cb6bf218cc2ea7c3071b706955233614a64b5d
berniTheHacker <57829799+berniTheHacker@users.noreply.github.com>
2024-11-28 17:36:00 +0100

    Added Register Basics

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -12,4 +12,5 @@ function registerRoutes(App $app)
 {
     $app->get('/', [HomeController::class, 'home']);
     $app->get('/about', [AboutController::class, 'about']);
+    $app->get('/register', [RegisterController::class, 'register']);
 }
